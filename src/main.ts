b3f1ac70af78a35adefe864f9e02910e14bf5a8b
Leul Gebremichael <leulgmichael@gmail.com>
2025-11-23 00:03:47 -1200

    changing on the main ts

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,8 @@ origin: [
   'http://localhost:5173',
   'https://customer-portal-111.onrender.com',
   "https://customer-portal-1.onrender.com",
-  "https://lmgsystem-tau.vercel.app/",
-  "https://lmgsystem-p7z3.vercel.app/",
+  "https://lmgsystem-tau.vercel.app",
+  "https://lmgsystem-p7z3.vercel.app",
   "https://lmg-system.onrender.com",
 "https://lmg-system-bfyr.onrender.com"
 ],
