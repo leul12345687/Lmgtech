316237ce395deb4c63be7f17098bb686cee25ce2
Leul Gebremichael <leulgmichael@gmail.com>
2026-02-27 19:37:00 +0300

    loading to the more abstract

--- src/ai_income-info/ai_income-info.module.ts
+++ src/ai_income-info/ai_income-info.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { AiService } from './ai_income-info.service';
 
