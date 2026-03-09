e4824b3c33cb79f6408fa9254bf1cbaf62171b36
Leul Gebremichael <leulgmichael@gmail.com>
2026-03-09 16:00:29 +0300

    integrating domain name of the smart merchant portal

--- src/main.ts
+++ src/main.ts
@@ -20,10 +20,11 @@ app.enableCors({
 origin: [
   'http://localhost:5173',
   'https://customer-portal-111.onrender.com',
-  "https://customer-portal-1.onrender.com",
-  "https://lmgsystem-tau.vercel.app",
-  "https://lmgsystem-p7z3.vercel.app",
-  "https://lmg-system.onrender.com",
+"https://customer-portal-1.onrender.com",
+"https://lmgsystem-tau.vercel.app",
+"https://smart-merchant-portal.onrender.com",
+"https://lmgsystem-p7z3.vercel.app",
+"https://lmg-system.onrender.com",
 "https://lmg-system-bfyr.onrender.com",
 "https://lmg-merchant-portal.vercel.app",
 "https://lmg-admin-portal.vercel.app"
